75698b290b0a4c357ee3d24d8a5a54be22326d9b
Eric Higdon <eric.s.higdon@gmail.com>
2025-05-20 20:05:42 -0400

    adjusted ingredient spacing

--- helpers.py
+++ helpers.py
@@ -131,3 +131,40 @@ def save_base64_image(base64_string, output_path):
         logger.debug(f'Image saved to {output_path}')
     except Exception as e:
         logger.exception(f'Error decoding or saving image')
+
+
+def wrap_text(text, font, width):
+    """Wrap text to fit inside a given width when rendered.
+
+    :param text: The text to be wrapped.
+    :param font: The font the text will be rendered in.
+    :param width: The width to wrap to.
+
+    """
+    text_lines = text.replace('\t', '    ').split('\n')
+    if width is None or width == 0:
+        return text_lines
+
+    wrapped_lines = []
+    for line in text_lines:
+        line = line.rstrip() + ' '
+        if line == ' ':
+            wrapped_lines.append(line)
+            continue
+
+        # Get the leftmost space ignoring leading whitespace
+        start = len(line) - len(line.lstrip())
+        start = line.index(' ', start)
+        while start + 1 < len(line):
+            # Get the next potential splitting point
+            next = line.index(' ', start + 1)
+            if font.size(line[:next])[0] <= width:
+                start = next
+            else:
+                wrapped_lines.append(line[:start])
+                line = line[start+1:]
+                start = line.index(' ')
+        line = line[:-1]
+        if line:
+            wrapped_lines.append(line)
+    return wrapped_lines
